062ef7ed43b7a4ff428b94c7a0e8577f8e4fd945
eckart invasor <eckart.inv3@gmail.com>
2018-05-28 19:42:28 -0500

    limpiando el proyecto

--- funcionesApi.js
+++ funcionesApi.js
@@ -2,21 +2,12 @@ var PathAPI = "https://ods.org.mx/v2/API/";
 
 	function getIndicadorSer(indicador,ser){
 	  var estados = [];
-	  console.log("Indicador",indicador," -> Serie",ser);
 		$.ajax({
 		  type: 'POST',
 		  url: PathAPI + "Valores/PorClaveSerie",
 		  data: {'PCveInd':indicador,'PAnoIni':'0', 'PAnoFin':'0', 'POrden':'DESC','PCveSer': ser , 'PIdioma':'ES'},
 		  success: function( data, textStatus, jqxhr ) {
-		  		//alert( "Exito" );
-				console.log(data);
-				console.log('------------------------------------- nuevo arreglo  --------------------');
-				console.log(data.Series);
-				console.log(data.Series[0]);
-				console.log(data.Series[0].Coberturas);
-				console.log(data.Series[0].Coberturas.length);
-
-
+		  		
 	    			var temporal = [];
 	    			temporal.push('Entidad');
 	    			for (var j = 0; j < data.Series[0].Coberturas[0].ValorDato.length; j++) {
@@ -32,16 +23,11 @@ var PathAPI = "https://ods.org.mx/v2/API/";
 	    					temporal.push(data.Series[0].Coberturas[i].ValorDato[j].Dato_ser);
 	    				}
 	    				estados.push(temporal);
-	    			}
-
-	    			console.log(estados);
-
+						}
+						
 	    			var codigo_indicador = data.Codigo_ind;
-	    			console.log(codigo_indicador);
 	    			var descripcion = data.Descrip_ind;
-	    			console.log(descripcion);
-
-	    			//inicio =  1;
+	    			
 	    			setTimeout(function(){$('#preloader').fadeOut('slow',function(){$(this).remove();});},3000);
 		  },
 		  async:false
